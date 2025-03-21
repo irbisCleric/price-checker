3daf5d8d7796920b038b63e12a93982b6d1836c6
Serhii K <serhii.kokhan5@gmail.com>
2025-03-21 15:07:39 +0100

    Changed config.

--- src/settings/price-checker-settings.js
+++ src/settings/price-checker-settings.js
@@ -11,24 +11,25 @@ const config = {
                 url: 'https://www.amazon.nl/-/en/Synology-DS423-bay-NAS-Device/dp/B0BXH389D1/',
                 selector: '.a-price-whole',
             },
-            {
-                store: 'Bol.com',
-                url: 'https://www.bol.com/nl/nl/p/synology-diskstation-ds423-nas-rack-intel-celeron-j4125-zwart/9300000143643524/',
-                selector: '.js_buy_block_prices .promo-price',
-            }
+            // TODO: Debug the Bol.com selector
+            // {
+            //     store: 'Bol.com',
+            //     url: 'https://www.bol.com/nl/nl/p/synology-diskstation-ds423-nas-rack-intel-celeron-j4125-zwart/9300000143643524/',
+            //     selector: '.main',
+            // }
         ],
-        // 'Seagate IronWolf 4TB': [
-        //     {
-        //         store: 'CoolBlue',
-        //         url: 'https://www.coolblue.nl/product/123456/seagate-ironwolf-4tb.html',
-        //         selector: '.js-sales-price-current',
-        //     },
-        //     {
-        //         store: 'AmazonNL',
-        //         url: 'https://example.com/product3',
-        //         selector: '#price-id', // Replace with the actual CSS selector for the price
-        //     },
-        // ],
+        'Seagate IronWolf 4TB': [
+            {
+                store: 'CoolBlue',
+                url: 'https://www.coolblue.nl/product/905240/seagate-ironwolf-4tb.html',
+                selector: '.product-order .js-sales-price-current',
+            },
+            {
+                store: 'AmazonNL',
+                url: 'https://www.amazon.nl/-/en/Seagate-IronWolf-Internal-Services-ST4000VNZ06/dp/B09NHV3CK9/',
+                selector: '.a-price-whole',
+            },
+        ],
     },
     twilio: {
         accountSid: 'your_account_sid',
